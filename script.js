b90d48929b2c78182f439478e905cf6c69d21c5e
Johnvilin-Sibina <johnvilinsibina@gmail.com>
2024-04-23 13:37:16 +0530

    Test cases are solved.

--- script.js
+++ script.js
@@ -7,39 +7,50 @@ container.className = "container";
 let row = document.createElement("div");
 row.className = "row";
 
+let title = document.createElement("h1");
+title.id = "title";
+title.innerHTML = "Rest Countries and Weather Using Fetch API";
+title.className = "text-center";
+
+container.append(title);
+document.body.append(container);
+
 function foo(data1){
     for(var i=0;i<data1.length;i++){
         let col = document.createElement("div");
-        col.className = "col-lg-4 col-sm-2";
-        col.innerHTML = `<div class="card" style="width: 18rem;">
-        <div class="card-header">${data1[i].name.common}</div>
-        
-        <img src=${data1[i].flags.png} class="card-img-top">
+        col.className = "col-sm-6 col-md-4 col-lg-4 col-xl-4";
+        col.innerHTML = `
+        <div class="card h-100">
+        <div class="card-header">
+        <h5 class = "card-title text-center"><b>${data1[i].name.common}</b></h5>
+        </div>
+        <div class="img-box">
+        <img src="${data1[i].flags.png}" class="card-img-top" alt="Country Flag Image">
+        </div>
         
         <div class="card-body">
-          <h5 class="card-title">Capital: ${data1[i].capital}</h5>
-          <h5 class="card-title">Region: ${data1[i].region}</h5>
-          <h5 class="card-title">Country Code: ${data1[i].cca3}</h5>          
-          <a href="#" class="btn btn-primary" onclick="button_click(event, ${data1[i].latlng[0]}, ${data1[i].latlng[1]})">Click for Weather</a>
+          <div class="card-text text-center"><b>Region:</b> ${data1[i].region}</div>
+          <div class="card-text text-center"><b>Capital:</b> ${data1[i].capital}</div>
+          <div class="card-text text-center"><b>Country Code:</b> ${data1[i].cca3}</div>          
+          <button  class="btn btn-primary" onclick="button_click(event, ${data1[i].latlng[0]}, ${data1[i].latlng[1]})">Click for Weather</button>
           <br>
           <span class="temperature-span"></span>
           </div>
         </div>`  
         
     row.append(col);
-    container.append(row);
-    document.body.append(container);
     }
 }
 
 function button_click(event, lat, lon){
   event.preventDefault();
-  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=`)
+  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=a8f110d087a43865625b26cfa1f87f8b
+  `)
       .then((data2)=>data2.json()).then((data3)=>{
         let temperature = data3.main.temp;
       let span = event.target.parentElement.querySelector('.temperature-span');
       span.innerText = `Temperature: ${temperature}`;
     })
-    }  
-
-
+    }
+    container.append(row);
+    document.body.append(container);
